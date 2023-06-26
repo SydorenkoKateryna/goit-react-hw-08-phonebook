03dfc0075b36e2eae917cd44487e0a821132f506
SydorenkoKateryna <sydorenko.k.ih@gmail.com>
2023-06-26 14:45:08 +0000

    Deploying to gh-pages from @ SydorenkoKateryna/goit-react-hw-08-phonebook@3f86f1bada655e0d881997167e8578c24b6256bf 🚀

--- static/js/519.ba0709c6.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[519],{519:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184),i=function(){return(0,n.jsx)("h2",{children:"RegisterForm"})},c=function(){return(0,n.jsxs)("div",{children:[(0,n.jsx)("title",{children:"Registration"}),(0,n.jsx)(i,{})]})}}}]);
-//# sourceMappingURL=519.ba0709c6.chunk.js.map
