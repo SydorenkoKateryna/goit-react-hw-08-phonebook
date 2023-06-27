32f0afe377a86a818b0fb5fee3fe69f21992d339
SydorenkoKateryna <sydorenko.k.ih@gmail.com>
2023-06-27 12:59:54 +0000

    Deploying to gh-pages from @ SydorenkoKateryna/goit-react-hw-08-phonebook@7ea0057c1be85d146a2e622ce8a5300c4c1ba413 🚀

--- static/js/415.69216286.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,n,t){t.r(n);var o,r=t(168),a=t(6444),c=t(3329),h=a.ZP.h1(o||(o=(0,r.Z)(["\n  font-weight: 500;\n  font-size: 48;\n  text-align: center;\n"])));n.default=function(){return(0,c.jsx)("main",{children:(0,c.jsx)(h,{children:"Phonebook welcome page "})})}}}]);
-//# sourceMappingURL=415.69216286.chunk.js.map
